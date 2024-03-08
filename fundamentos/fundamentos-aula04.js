749da83a99d493d75aa1c5b10ee2f2d1729e80b4
Gustavo-web-dev <tavintwd@gmail.com>
2024-03-07 21:56:06 -0300

    tarefa 01

---
+++ fundamentos/fundamentos-aula04.js
@@ -0,0 +1,29 @@
+//Repetições LOPP
+
+//laço for
+ //Inicialozação: condição: incremento {
+
+ // }
+
+// for(let contador = 1; contador <= 10; contador = contador + 1) {
+console.log("Loop")
+// }
+
+// Função
+function executa() {
+    console.log("Exeutando função")
+}
+executa()
+
+function soma(numA, numB) {
+        console.log(numA + numB)
+    
+}
+soma(2, 2)
+
+// função na variavel
+const soma2 = function(numA, numB){
+console.log(numA + numB)
+}
+
+soma2(2, 10)
