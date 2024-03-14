5b6306eeba7e50e3b8780b4c58dd7ccce590c3f2
Gustavo-web-dev <tavintwd@gmail.com>
2024-03-14 19:57:40 -0300

    Esquema de modulos padrão nodeJS

---
+++ api-exemplo/calculadora.js
@@ -0,0 +1,26 @@
+const somar = function(numA, numB) {
+    return numA + numB
+}
+
+const subtrair = function(numA, numB) {
+    return numA - numB
+}
+
+const multiplicar = function(numA, numB) {
+    return numA * numB
+}
+
+const dividir = function(numA, numB) {
+    return numA / numB
+}
+
+
+const soma = function(numA, numB) {
+    return numA + numB
+}
+
+module.exports = {
+    somar, subtrair, multiplicar, dividir
+}
+
+
